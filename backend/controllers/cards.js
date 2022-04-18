f87688f55af97563e05a46d832a9571cf855bd6e
Данила <suhorukovdanila@yandex.ru>
2022-04-18 12:19:06 +0300

    Add files via upload

---
+++ backend/controllers/cards.js
@@ -0,0 +1,91 @@
+const Cards = require('../model/card');
+const BadRequestError = require('../errors/badRequestError');
+const NotFoundError = require('../errors/notFoundError');
+const ForbiddenError = require('../errors/forbiddenError');
+
+module.exports.getCards = (req, res, next) => {
+  Cards.find({})
+    .then((cards) => res.status(200).send(cards))
+    .catch(next);
+};
+
+module.exports.createCard = (req, res, next) => {
+  const { name, link } = req.body;
+  const ownerId = req.user._id;
+  Cards.create({ name, link, owner: ownerId })
+    .then((card) => res.status(200).send(card))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        next(new BadRequestError('Переданы некорректные данные.'));
+      } else {
+        next(err);
+      }
+    });
+};
+
+module.exports.deleteCardById = (req, res, next) => {
+  Cards.findById(req.params.cardId)
+    .then((card) => {
+      if (!card) {
+        throw NotFoundError('Карточка не найдена');
+      }
+      if (!card.owner.toString() === req.user._id.toString()) {
+        throw ForbiddenError('Карточку создал другой пользователь. Невозможно удалить.');
+      }
+      return card.remove();
+    })
+    .then((card) => {
+      res.status(200).send(card);
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        next(new BadRequestError('Переданы некорректные данные.'));
+      } else {
+        next(err);
+      }
+    });
+};
+
+module.exports.addLike = (req, res, next) => {
+  Cards.findByIdAndUpdate(
+    req.params.cardId,
+    { $addToSet: { likes: req.user._id } },
+    { new: true },
+  )
+    .then((card) => {
+      if (card) {
+        res.status(200).send(card);
+      } else {
+        next(new NotFoundError('Пользователь не найден'));
+      }
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        next(new BadRequestError('Переданы некорректные данные.'));
+      } else {
+        next(err);
+      }
+    });
+};
+
+module.exports.removeLike = (req, res, next) => {
+  Cards.findByIdAndUpdate(
+    req.params.cardId,
+    { $pull: { likes: req.user._id } },
+    { new: true },
+  )
+    .then((card) => {
+      if (card) {
+        res.status(200).send(card);
+      } else {
+        next(new NotFoundError('Пользователь не найден'));
+      }
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        next(new BadRequestError('Переданы некорректные данные.'));
+      } else {
+        next(err);
+      }
+    });
+};
