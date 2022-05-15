ed308e7af3f499c62c4da782daf2b220e644bcca
Данила <suhorukovdanila@yandex.ru>
2022-05-15 14:28:21 +0300

    Add files via upload

---
+++ backend/app.js
@@ -0,0 +1,53 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const { errors } = require('celebrate');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cors = require('./middlewares/cors');
+const router = require('./routes/index');
+const { login, createUser, signout } = require('./controllers/users');
+const errorHandler = require('./middlewares/errorHandler');
+const { userValidation, loginValidation } = require('./middlewares/validationJoi');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+
+const { PORT = 3000 } = process.env;
+const app = express();
+app.use(cors({
+  origin: [
+    'https://dsukh.nomoredomains.work/',
+    'http://dsukh.nomoredomains.work/', 
+    'http://localhost:3000',
+  ],
+  credentials: true,
+}));
+app.use(cors());
+
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(requestLogger);
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
+app.post('/signin', loginValidation, login);
+app.post('/signup', userValidation, createUser);
+app.get('/signout', signout);
+
+app.use(router);
+app.use(errorLogger);
+app.use(errors());
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  console.log(`listen port ${PORT}`);
+});
