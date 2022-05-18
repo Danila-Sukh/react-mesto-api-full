44cc4ca15733a9f615e7fa2d4407503e25f22ee4
Данила <suhorukovdanila@yandex.ru>
2022-05-18 23:04:28 +0300

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 class Api {
-  constructor({ baseUrl, headers }) {
-    this.url = baseUrl;
-    this.headers = headers;
+  constructor(options) {
+    this.url = options.baseUrl;
+    this.headers = options.headers;
   }
 
   _handleFetch(res) {
