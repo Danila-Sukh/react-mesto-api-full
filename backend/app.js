04ccd5cfe3ca309007222e7e8ca351c61ad569b4
Данила <suhorukovdanila@yandex.ru>
2022-05-18 23:21:46 +0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -14,8 +14,10 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { PORT = 3000 } = process.env;
 const app = express();
 
-
- app.use(cors);
+const options = {
+  credentials: true,
+};
+ app.use('*', cors(options));
 
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
