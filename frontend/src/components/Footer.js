37323b251be2ad8815c76e5f361e3f7335dbc6dd
Данила <suhorukovdanila@yandex.ru>
2022-05-20 12:51:17 +0300

    Delete frontend/src directory

--- frontend/src/components/Footer.js
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="footer page__footer">
-      <p className="footer__copyrights">&copy; 2021 Mesto Russia</p>
-    </footer>
-  );
-}
