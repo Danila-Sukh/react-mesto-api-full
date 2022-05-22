b3160a560a21d8fd5b1fd204963cc7a94276be0b
Данила <suhorukovdanila@yandex.ru>
2022-05-22 19:16:25 +0300

    Add files via upload

---
+++ frontend/src/utils/AuthApi.js
@@ -0,0 +1,54 @@
+const url = "https://api.dsukh.nomoredomains.work";
+
+class AuthApi {
+  constructor({ url }) {
+    this.url = url;
+  }
+
+  register({ email, password }) {
+    return fetch(this.url + "/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    }).then((response) => {
+      return this._getResponseData(response);
+    });
+  }
+
+  authorization({ email, password }) {
+    return fetch(this.url + "/sign-in", {
+      method: "POST",
+      credentials: 'include',
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    }).then((response) => {
+      return this._getResponseData(response);
+    });
+  }
+
+  getContent(token) {
+    return fetch(this.url + "/users/me", {
+      method: "GET",
+      credentials: 'include',
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    }).then((response) => {
+      return this._getResponseData(response);
+    });
+  }
+
+  _getResponseData(res) {
+    if (!res.ok) {
+      return Promise.reject(`Ошибка: ${res.status}`);
+    }
+    return res.json();
+  }
+}
+
+export const apiAuth = new AuthApi(baseUrl);
