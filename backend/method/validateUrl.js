03f1f2a9c81567851d0ec7d23de69863c4b89bf5
Данила <suhorukovdanila@yandex.ru>
2022-05-23 13:59:09 +0300

    Delete backend directory

--- backend/method/validateUrl.js
+++
@@ -1,9 +0,0 @@
-const validator = require('validator');
-
-module.exports.validateUrl = (url) => {
-  const validate = validator.isURL(url);
-  if (validate) {
-    return url;
-  }
-  throw new Error('URL validation err');
-};
